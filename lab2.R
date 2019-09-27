9d6708804c8514fc67cb05fff66641f3bb0d9a1e
Franco Patricio Leal Guarda <flealg@DIINF.LAN>
2019-09-27 16:10:57 -0300

    proximity and importance

--- lab2.R
+++ lab2.R
@@ -1,6 +1,6 @@
 require('randomForest')
-
-path = "~/Documentos/2-2019/TMDA/tmdaLab2/breast-cancer-wisconsin.data"
+require('MASS')
+path = "~/TMDA/tmdaLab2_randomForest/breast-cancer-wisconsin.data"
 #path = "~/Escritorio/USACH/Topicos/Taller de mineria de datos avanzada/tmdaLab1/breast-cancer-wisconsin.data"
 data =  read.table(path,sep=",", na.strings = c("?"))
 
@@ -22,10 +22,10 @@ data.2 = data.without.na[,2:11]
 smp_size = floor(0.632*nrow(data.2))
 set.seed(12345)
 
-#train_ind = sample(seq_len(nrow(data.2)),size = smp_size)
+train_ind = sample(seq_len(nrow(data.2)),size = smp_size)
 
-#data.train = data.2[train_ind,]
-#data.test = data.2[-train_ind,]
+data.train = data.2[train_ind,]
+data.test = data.2[-train_ind,]
 data.2$Class = as.factor(data.2$Class)
 #600 arboles es mejor que 700
 data.rf = randomForest(Class ~ ., data = data.2, ntree= 600,importance=TRUE, proximity = TRUE)
@@ -34,18 +34,34 @@ print(data.rf)
 round(importance(data.rf),2)
 varImpPlot(data.rf)
 
-data.3 = subset(data.2 , select = -mitoses)
-data.rf.2 = randomForest(Class ~ ., data = data.3, ntree= 600,importance=TRUE, proximity = TRUE)
-print(data.rf.2)
-round(importance(data.rf.2),2)
-varImpPlot(data.rf.2)
+data.mds = cmdscale(1-data.rf$proximity,eig=TRUE)
+op = par(pty="s")
+pairs(cbind(iris[,1:4], iris.mds$points), cex=0.6, 
+      gap=0,
+      col=c("red", "green")[as.numeric(data.2$Class)],
+      main="BCW Data: Predictors and MDS of Proximity Based on RandomForest")
 
 
-data.4 = subset(data.3,select=-adhesion)
-data.rf.3 = randomForest(Class ~ ., data = data.4, ntree= 600,importance=TRUE, proximity = TRUE)
-print(data.rf.3)
-round(importance(data.rf.3),2)
-varImpPlot(data.rf.3)
+plot(data.rf)
+legend("bottomright", colnames(data.rf$err.rate),col=1:4,cex=0.8,fill=1:4)
+
+parcoord(data.2[,1:9],var.label = TRUE,col=c("red", "green")[as.numeric(data.2$Class)])
+legend("bottomright",legend = c("Benigno", "Maligno"),fill=2:3)
+
+
+
+#data.3 = subset(data.2 , select = -mitoses)
+#data.rf.2 = randomForest(Class ~ ., data = data.3, ntree= 600,importance=TRUE, proximity = TRUE)
+#print(data.rf.2)
+#round(importance(data.rf.2),2)
+#varImpPlot(data.rf.2)
+
+
+#data.4 = subset(data.3,select=-adhesion)
+#data.rf.3 = randomForest(Class ~ ., data = data.4, ntree= 600,importance=TRUE, proximity = TRUE)
+#print(data.rf.3)
+#round(importance(data.rf.3),2)
+#varImpPlot(data.rf.3)
 #regularizacion:  parsimonia (BIC)
 #generalizacion (datos nuevos)
 
